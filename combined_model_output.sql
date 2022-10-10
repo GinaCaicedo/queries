6670769f4623e8564ded0d1498711c498c21572c
caicedo <gina.caicedo@here.com>
2022-10-10 16:37:07 -0500

    update

--- combined_model_output.sql
+++ combined_model_output.sql
@@ -2,6 +2,7 @@
 
 create table combined_model_output
 select lm.observation_id, lm.link_pvid, lm.end_of_link,rm.valid_probability,rm.valid_prediction
-from  link_mataching observation_id, link_pvid as lm
+from  link_mataching as lm
 join full_data _prediction_results_model1 as rm
 on lm.observation_id=prediction.Id
+vombined_model_output.sql
